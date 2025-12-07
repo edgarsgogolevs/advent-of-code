fb1f4be5c286f6b1d2a9b48ffa577c9afe0b47d5
Edgars Gogolevs <edgars.gogolevs@gmail.com>
2025-12-07 23:05:35 +0200

    idea for optimization

--- 2025/2/main.go
+++ 2025/2/main.go
@@ -11,6 +11,29 @@ import (
 // and return their sum
 func AnalyzeRange(rng string) int {
 	// TODO: must be some neat optimization, that reduces nested loop (currently 4 O_o)
+	//
+	// TODO: possible optimization:
+	// as number should consist only of repeating sequences,
+	// we first longest sequence of not repeating char, for example,
+	// in 123123123 it would be 123
+	// then check if rest of string is repeating the crap
+	//
+	// in 123123123
+	// take 1
+	// check if next seq of same len is the same
+	// 1 != 2
+	// take 12
+	// 12 != 31
+	// take 123
+	// 123 == 123
+	// start with last index + 1 and take slices of len of seq until the end
+	// in this case take the last 123
+	// 123 == 123 => invalid ID
+	//
+	// TODO: there is also a trick of doubling the string getting rid of first and last char
+	// and checking if original string is a substring of this one, if is => repeating seq
+	//
+	// TODO: implement the trick and compare times of solutions
 	sum := 0
 	parts := strings.SplitN(rng, "-", 2)
 
@@ -26,6 +49,10 @@ func AnalyzeRange(rng string) int {
 
 	for num := start; num <= end; num++ {
 		sNum := strconv.Itoa(num)
+		if len(sNum) == 1 {
+			// single digit cannot repeat
+			continue
+		}
 
 		// part one solution
 		// if len(sNum)%2 != 0 {
