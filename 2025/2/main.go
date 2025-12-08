cd5a3e689ea5df31e24983a285dbf005034a7914
Edgars Gogolevs <edgars.gogolevs@gmail.com>
2025-12-08 09:39:07 +0200

    try some optimizations

--- 2025/2/main.go
+++ 2025/2/main.go
@@ -5,35 +5,17 @@ import (
 	"os"
 	"strconv"
 	"strings"
+	"time"
 )
+
+func IsRepeatedPattern(s string) bool {
+	doubled := s + s
+	return strings.Contains(doubled[1:len(doubled)-1], s)
+}
 
 // Check how many invalid ids are there in the range
 // and return their sum
 func AnalyzeRange(rng string) int {
-	// TODO: must be some neat optimization, that reduces nested loop (currently 4 O_o)
-	//
-	// TODO: possible optimization:
-	// as number should consist only of repeating sequences,
-	// we first longest sequence of not repeating char, for example,
-	// in 123123123 it would be 123
-	// then check if rest of string is repeating the crap
-	//
-	// in 123123123
-	// take 1
-	// check if next seq of same len is the same
-	// 1 != 2
-	// take 12
-	// 12 != 31
-	// take 123
-	// 123 == 123
-	// start with last index + 1 and take slices of len of seq until the end
-	// in this case take the last 123
-	// 123 == 123 => invalid ID
-	//
-	// TODO: there is also a trick of doubling the string getting rid of first and last char
-	// and checking if original string is a substring of this one, if is => repeating seq
-	//
-	// TODO: implement the trick and compare times of solutions
 	sum := 0
 	parts := strings.SplitN(rng, "-", 2)
 
@@ -51,6 +33,13 @@ func AnalyzeRange(rng string) int {
 		sNum := strconv.Itoa(num)
 		if len(sNum) == 1 {
 			// single digit cannot repeat
+			continue
+		}
+		// in double digit numbers only multiples of 11 are repeated pattern
+		if len(sNum) == 2 && num%11 != 0 {
+			continue
+		}
+		if len(sNum) == 3 && num%111 != 0 {
 			continue
 		}
 
@@ -72,11 +61,13 @@ func AnalyzeRange(rng string) int {
 			seq := sNum[:seqEndIdx]
 			rest := sNum[seqEndIdx:]
 
+			// no need to check the pattern if string is not dividable in these patterns
+			if len(rest)%len(seq) != 0 {
+				isInvalid = false
+				continue
+			}
+
 			for i := 0; i < len(rest); i += len(seq) {
-				if i+len(seq) > len(rest) {
-					isInvalid = false
-					break
-				}
 				if seq != rest[i:i+len(seq)] {
 					isInvalid = false
 					break
@@ -85,7 +76,7 @@ func AnalyzeRange(rng string) int {
 			}
 
 			if isInvalid {
-				fmt.Printf("ID INVALID: invalid id: %s\n", sNum)
+				// fmt.Printf("ID INVALID: invalid id: %s\n", sNum)
 				sum += num
 				break
 			}
@@ -103,13 +94,66 @@ func Solve(input string) int {
 	return solution
 }
 
+func AnalyzeQuicker(rng string) int {
+	sum := 0
+	parts := strings.SplitN(rng, "-", 2)
+
+	start, err := strconv.Atoi(parts[0])
+	if err != nil {
+		panic(fmt.Sprintf("unable to parse start: %v\n%s", parts[0], err))
+	}
+
+	end, err := strconv.Atoi(parts[1])
+	if err != nil {
+		panic(fmt.Sprintf("unable to parse end: %v\n%s", parts[1], err))
+	}
+
+	for num := start; num <= end; num++ {
+		sNum := strconv.Itoa(num)
+		if len(sNum) == 1 {
+			// single digit cannot repeat
+			continue
+		}
+		// in double digit numbers only multiples of 11 are repeated pattern
+		if len(sNum) == 2 && num%11 != 0 {
+			continue
+		}
+		if len(sNum) == 3 && num%111 != 0 {
+			continue
+		}
+		if IsRepeatedPattern(sNum) {
+			sum += num
+		}
+	}
+	return sum
+}
+
+func SolveQuicker(input string) int {
+	solution := 0
+	for value := range strings.SplitSeq(input, ",") {
+		solution += AnalyzeQuicker(strings.Trim(value, " \n"))
+	}
+	return solution
+}
+
+// Quicker solution did not turn out to be quicker,
+// I think this is because it still performs linear contains operation inside 2 nested loops
+// and strings we are checking are relatively small
 func main() {
 	// TODO: reading thing byte by byte would be cool and efficient
 	byteContent, err := os.ReadFile("input")
 	if err != nil {
-		panic(fmt.Sprintf("Unable to read file: %e", err))
+		panic(fmt.Sprintf("Unable to read file: %s", err))
 	}
-	solution := Solve(string(byteContent))
+	content := string(byteContent)
 
-	fmt.Printf("The solution is %d\n", solution)
+	start := time.Now()
+	solution := Solve(content)
+	elapsed := time.Since(start)
+	fmt.Printf("The solution is %d; Took: %s\n", solution, elapsed)
+
+	start = time.Now()
+	solution = SolveQuicker(content)
+	elapsed = time.Since(start)
+	fmt.Printf("The solution is %d; Took: %s\n", solution, elapsed)
 }
